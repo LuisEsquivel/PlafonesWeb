ab9548d3202f5eb3f75c8046dfdf7e2626478037
LuisEsquivel <32809166+LuisEsquivel@users.noreply.github.com>
2020-03-22 18:29:17 -0600

    CAMBIOS EN NUMERO DE CARACTERES DE INPUTS DE CONTACTO Y VALIDACION DE SOLO NUMEROS EN EL INPUT DE TELEFONO

---
+++ PlafonesWeb/Scripts/Generales.js
@@ -0,0 +1,42 @@
+﻿
+
+
+
+  // $(document).ready(function (){
+  //      $('.soloNumeros').keyup(function () {
+		//	this.value = (this.value + '').replace(/[^0-9]/g, '');
+		//});
+  // });
+
+jQuery(document).ready(function () {
+	jQuery('.soloNumeros').keypress(function (tecla) {
+		if (tecla.charCode < 48 || tecla.charCode > 57) return false;
+	});
+});
+
+
+
+
+   $('#textArea').prop("required", true);
+
+
+
+
+function validar() {
+
+	//Almacenamos los valores
+	nombre = $('#telefono').val();
+
+	//Comprobamos la longitud de caracteres
+	if (nombre.length < 8) {
+		$('#telefono').prop("required", true);
+	}
+	else {
+		$('#telefono').prop("required", false);
+
+	}
+
+}
+
+
+
